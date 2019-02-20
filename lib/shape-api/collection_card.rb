7681e3404500c7c34b73223e497b2aaa8baa15c4
kaptron <dkaplan@ideo.com>
2019-02-20 15:56:24 -0800

    Refactor CollectionCard yet again to simplify it
    - now the usage is to just call ShapeApi::CollectionCard.create and pass 
    in normal card + item/collection params. It seemed that creating a bunch 
    of helper methods that were just very light wrappers on the normal API 
    was creating too much overhead when the normal API is simple enough as 
    is.

--- lib/shape-api/collection_card.rb
+++ lib/shape-api/collection_card.rb
@@ -1,39 +1,20 @@
 module ShapeApi
   class CollectionCard < Base
-    def self.build(attributes: {}, item_attributes: {}, collection_attributes: {})
-      params = {
-        order: 0,
-        height: 1,
-        width: 1,
-      }.merge(attributes)
-      params[:item_attributes] = item_attributes if item_attributes.present?
-      params[:collection_attributes] = collection_attributes if collection_attributes.present?
-      new(params)
-    end
+    def self.create_with_text_item(**params)
+      unless params[:item_attributes].present?
+        card = new
+        card.errors.add(:item_attributes, 'required')
+        return card
+      end
 
-    def self.create_with_text_item(content:, card_attributes: {})
-      text_data = { ops: [insert: "#{content}\n"] }
-      card = build(
-        attributes: card_attributes,
-        item_attributes: {
-          type: 'Item::TextItem',
-          content: content,
-          text_data: text_data,
-        },
-      )
-      card.save
-      card
-    end
-
-    def self.create_with_collection(name:, card_attributes: {})
-      card = build(
-        attributes: card_attributes,
-        collection_attributes: {
-          name: name,
-        },
-      )
-      card.save
-      card
+      params[:item_attributes][:type] ||= 'Item::TextItem'
+      content = params[:item_attributes][:content]
+      if content.present?
+        params[:item_attributes][:data_content] = {
+          ops: [insert: "#{content}\n"],
+        }
+      end
+      create(params)
     end
   end
 end
