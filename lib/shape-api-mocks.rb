f462e0c0fbda48ae9a871d8f91f77f8897ce00a9
Josh Schwartzman <josh@dreamseedo.org>
2019-02-25 13:29:41 -0800

    Refactor mocks to be more DRY

--- lib/shape-api-mocks.rb
+++ lib/shape-api-mocks.rb
@@ -1,130 +1,94 @@
 module ShapeApiMocks
-  def shape_api_collection_double(where: nil)
-    allow(ShapeApi::Collection).to receive(:where).and_return(
-      where || [shape_api_collection_instance],
+  # To include these, add to your spec helper:
+  #   require 'shape-api-mocks'
+  #
+  # In your tests, you can register all models that you'd like mocked
+  # in before { } block, such as before { shape_api_register_double('Collection') }
+  #
+  # That will mock the ShapeApi::Collection class,
+  # adding default return values for where/new/create methods.
+  #
+  # e.g.
+  #
+  # let(:collection_instance) { double('ShapeApi::Collection', external_id: 45) }
+  #
+  # before do
+  #   shape_api_register_double('ShapeApi::Item')
+  #   shape_api_register_double('ShapeApi::Collection',
+  #                             create: collection_instance)
+  # end
+  #
+  # it 'should return values' do
+  #   expect(ShapeApi::Collection).to receive(:create).and_return(collection_instance)
+  # end
+  #
+
+  def shape_api_register_double(model_name, custom_instance_doubles = {})
+    klass = model_name.safe_constantize
+    underscore_klass = klass.model_name.param_key
+
+    # Check if there are custom instance attrs defined for this model,
+    # otherwise use default instance attrs
+    default_instance_attrs = if defined?(custom_instance_attrs)
+                               send(custom_instance_attrs)
+                             else
+                               shape_api_default_instance_attrs(klass)
+                             end
+
+    # Define an instance double for this model
+    define_singleton_method "#{underscore_klass}_instance_double" do
+      double(
+        model_name,
+        default_instance_attrs,
+      )
+    end
+
+    # Defines method that matches lowercase class name,
+    # e.g. `shape_api_collection_double`
+    #
+    # You can then pass params to be used for default return values
+    #
+    define_singleton_method "#{underscore_klass}_double" do
+      instance_double = send("#{underscore_klass}_instance_double")
+
+      allow(klass).to receive(:where).and_return(
+        custom_instance_doubles[:where] || [instance_double],
+      )
+
+      allow(klass).to receive(:new).and_return(
+        custom_instance_doubles[:new] || instance_double,
+      )
+
+      allow(klass).to receive(:create).and_return(
+        custom_instance_doubles[:create] || instance_double,
+      )
+    end
+  end
+
+  # Custom Instance Attributes
+  #
+  # Define any models that have custom instance attribute keys
+  #
+
+  def shape_api_organization_instance_attrs
+    default_api_instance_attrs(
+      ShapeApi::Organization,
+    ).merge(
+      current_user_collection_id: 1,
     )
   end
 
-  def shape_api_collection_card_double(where: nil, new: nil)
-    allow(ShapeApi::CollectionCard).to receive(:where).and_return(
-      where || [shape_api_collection_card_item_instance],
-    )
-    allow(ShapeApi::CollectionCard).to receive(:new).and_return(
-      new || shape_api_collection_card_item_instance,
-    )
-    allow(ShapeApi::CollectionCard).to receive(:create_with_text_item).and_return(
-      shape_api_collection_card_item_instance,
-    )
-  end
+  # Default Instance Attributes
+  # These will be present on all instance doubles
 
-  def shape_api_role_double(where: nil, new: nil)
-    allow(ShapeApi::Role).to receive(:create).and_return(
-      true,
-    )
-    allow(ShapeApi::Role).to receive(:new).and_return(
-      new || shape_api_role_instance,
-    )
-    allow(ShapeApi::Role).to receive(:where).and_return(
-      where || [shape_api_role_instance],
-    )
-  end
-
-  def shape_api_group_double(where: nil, new: nil)
-    allow(ShapeApi::Group).to receive(:create).and_return(
-      true,
-    )
-    allow(ShapeApi::Group).to receive(:where).and_return(
-      where || [shape_api_group_instance],
-    )
-    allow(ShapeApi::Group).to receive(:new).and_return(
-      new || shape_api_group_instance,
-    )
-  end
-
-  def shape_api_organization_double(where: nil)
-    allow(ShapeApi::Organization).to receive(:where).and_return(
-      where || [shape_api_organization_instance],
-    )
-  end
-
-  def shape_api_organization_instance(attrs = {})
-    double('ShapeApi::Organization',
-           {
-             id: 1,
-             external_id: 'CreativeDiffOrg_1',
-             current_user_collection_id: 1,
-           }.deep_merge(attrs),
-          )
-  end
-
-  def shape_api_group_instance(attrs = {})
-    double('ShapeApi::Group',
-           {
-             id: 1,
-             external_id: 'CreativeDiffBuGroup_1',
-             errors: [],
-             save: true,
-           }.deep_merge(attrs))
-  end
-
-  def shape_api_role_instance(attrs = {})
-    double('ShapeApi::Role',
-           {
-             id: 1,
-             name: 'editor',
-             errors: [],
-             groups: [],
-             users: [],
-             save: true,
-             destroy: true,
-           }.deep_merge(attrs))
-  end
-
-  def shape_api_collection_instance(attrs = {})
-    double('ShapeApi::Collection',
-           {
-             new: true,
-             save: true,
-             update_attributes: true,
-             external_id: 'CreativeDiffCollRecord_1',
-             errors: [],
-             id: 1,
-           }.deep_merge(attrs))
-  end
-
-  def shape_api_item_instance(attrs = {})
-    double('ShapeApi::Item',
-           {
-             new: true,
-             save: true,
-             update_attributes: true,
-             errors: [],
-             external_id: 'CreativeDiffItemRecord_1',
-             id: 1,
-           }.deep_merge(attrs))
-  end
-
-  def shape_api_collection_card_item_instance(attrs = {})
-    double('ShapeApi::CollectionCard',
-           shape_api_collection_card_attrs.deep_merge(
-             record: shape_api_item_instance,
-           ).deep_merge(attrs))
-  end
-
-  def shape_api_collection_card_collection_instance(attrs = {})
-    double('ShapeApi::CollectionCard',
-           shape_api_collection_card_attrs.deep_merge(
-             record: shape_api_collection_instance,
-           ).deep_merge(attrs))
-  end
-
-  def shape_api_collection_card_attrs
+  def shape_api_default_instance_attrs(klass)
     {
-      new: true,
-      update_attributes: true,
       id: 1,
-      save: true,
+      external_id: "My#{klass}_1",
       errors: [],
+      new: true,
+      save: true,
+      update_attributes: true,
     }
   end
 end
