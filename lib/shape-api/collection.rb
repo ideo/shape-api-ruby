e9a468974a962bc39a2c46c2afdaa936ced226bc
Josh Schwartzman <josh@dreamseedo.org>
2019-06-19 09:45:49 -0700

    Add custom methods to endpoints on Collection

--- lib/shape-api/collection.rb
+++ lib/shape-api/collection.rb
@@ -1,5 +1,14 @@
 module ShapeApi
   class Collection < Base
     include ShapeApi::Resourceable
+
+    # POST /collections/create_template
+    custom_endpoint :create_template, on: :collection, request_method: :post
+
+    # POST /collections/:id/clear_collection_cover
+    custom_endpoint :clear_collection_cover, on: :member, request_method: :post
+
+    # POST /collections/:id/restore_permissions
+    custom_endpoint :restore_permissions, on: :member, request_method: :post
   end
 end
